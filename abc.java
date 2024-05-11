7b18800a9e35810653393eac659d86e315c7510d
Hemant Shukla <hbshukla@gmail.com>
2024-05-12 00:20:43 +0530

    added new files

---
+++ abc.java
@@ -0,0 +1,24 @@
+package Java;
+class abc
+{
+   
+    public static void main(String args[]) 
+    /**
+    <<public>>    : is a access modifier & can be access out of the class. 
+    <<Static>>    : Class does not need to create an object to access the static method.
+    <<void>>      : Method will not return any value. 
+    <<main>>      : is the name of main method
+    <<String>>    : is a predefined class
+    <<args[]>>    : It's a variable of type array.
+    <<String args[]>> : called as command line argument
+     */
+
+{
+    System.out.println ("1st Program");
+    //<<System>> : is a predefined class in java.lang package
+    //<<out>> : is a static variable defined in class "System"
+    //<<print>> is the function of "System" class
+
+}
+
+}
