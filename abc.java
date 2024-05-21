2f9d3ac7aeedb1443a58b0f041b100eeee31d2c2
Hemant Shukla <hbshukla@gmail.com>
2024-05-21 19:51:26 +0530

    NM

--- abc.java
+++ abc.java
@@ -22,14 +22,42 @@ package Java;
 {
     System.out.println ("1st Program");
     
-    int a=4;
-    int b=2;
+    int a=7;
+    int b=18;
 
-    b +=a; //b=b+a so b = 2+4 = 6
-    int b1 = a++;
-System.out.println("Value of b is " + b );
-System.out.println("Value of b1 is " + b1 );
+    //b +=a; //b=b+a so b = 2+4 = 6
+    //int b1 = ++a;
+    System.out.println("Value of a is " + a );
+    System.out.println("Value of b is " + b );
+   // System.out.println("Value of b1 is " + b1 );
+    System.out.println("Value of a is " + a );
+    System.out.println("b%a =" + b%a);
+    System.out.println("b%a =" + a%b);
+    //System.out.println("b%a =" + a^b);
 
+
+
+/**In this loop, result *= base is equivalent to result = result * base. 
+  It means that result is multiplied by base, and the result is stored back in result. 
+  This operation is repeated for each iteration of the loop, effectively calculating 
+ 2^8*/
+int base = 2;
+int exponent = 8;
+int result = 1;
+
+for (int i = 0; i < exponent; i++) {
+    result *= base;
 }
 
+System.out.println(result);
+
+boolean x = true;
+boolean y = false;
+
+boolean result1 = x | y; // Logical OR operation
+
+System.out.println(result1); // Output: true
+
+
+}
 }
---
+++ logical_Operators.java
@@ -0,0 +1,17 @@
+package Java;
+/**
+ * logical_Operators
+ */
+class logical_Operators 
+{
+public static void main(String[] args)
+ {
+    boolean a = true;
+    boolean b = false;
+System.out.println(a|b); // Logical OR operation
+
+//----------------------------------------------
+
+System.out.println(a & b);
+} 
+}
