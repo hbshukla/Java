1859b9d9a1e0c393fe678fcc36fa87bfe95711c8
Hemant Shukla <hbshukla@gmail.com>
2024-05-21 12:19:02 +0530

    modified

--- abc.java
+++ abc.java
@@ -1,9 +1,5 @@
 package Java;
-class abc
-{
-   
-    public static void main(String args[]) 
-    /**
+/**
     <<public>>    : is a access modifier & can be access out of the class. 
     <<Static>>    : Class does not need to create an object to access the static method.
     <<void>>      : Method will not return any value. 
@@ -13,11 +9,23 @@ class abc
     <<String args[]>> : called as command line argument
      */
 
+//<<System>> : is a predefined class in java.lang package
+//<<out>> : is a static variable defined in class "System"
+//<<print>> is the function of "System" class
+
+
+     class abc
+{
+   
+    public static void main(String args[]) 
+
 {
     System.out.println ("1st Program");
-    //<<System>> : is a predefined class in java.lang package
-    //<<out>> : is a static variable defined in class "System"
-    //<<print>> is the function of "System" class
+    
+    int a=4;
+    int b=2;
+
+    b +=a; //b=b+a so b = 2+4 = 6
 
 }
 
