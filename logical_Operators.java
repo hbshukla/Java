0cd8885f57807adaef4814935032d13e6d498ea5
Hemant Shukla <hbshukla@gmail.com>
2024-05-22 18:53:17 +0530

    M

--- logical_Operators.java
+++ logical_Operators.java
@@ -11,10 +11,7 @@ public static void main(String[] args)
 int p=15 ;
 int q=10;
     System.out.println(a|b); // Logical OR operation
-System.out.println(+(p|q));
-
-//----------------------------------------------
-
+System.out.println(+(q & p));
 System.out.println(a & b);
 } 
 }
